c23e2eb866c04e918820a276079bf0399999ea56
A.Bond <alexey.bond.94.55@gmail.com>
2017-04-22 15:26:36 +0600

    Add something (not) cool.

---
+++ android/assets/shaders/postfx_fragment_mblurPrev.glsl
@@ -0,0 +1,13 @@
+#ifdef GL_ES
+    precision mediump float;
+#endif
+
+varying vec4 v_color;
+varying vec2 v_texCoords;
+uniform sampler2D u_texture;
+uniform mat4 u_projTrans;
+
+void main() {
+        vec4 color = v_color * texture2D(u_texture, vec2(v_texCoords.x, 1.0 - v_texCoords.y));
+        gl_FragColor = color;
+}
--- core/src/com/github/oldnpluslusteam/old_38_game/TheGame.java
+++ core/src/com/github/oldnpluslusteam/old_38_game/TheGame.java
@@ -2,32 +2,139 @@ package com.github.oldnpluslusteam.old_38_game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import static com.badlogic.gdx.graphics.GL20.GL_ONE;
+import static com.badlogic.gdx.graphics.GL20.GL_ONE_MINUS_SRC_ALPHA;
+import static com.badlogic.gdx.graphics.GL20.GL_SRC_ALPHA;
+import static com.badlogic.gdx.graphics.glutils.HdpiUtils.glViewport;
 
 public class TheGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    static final int VP_WIDTH = 1366, VP_HEIGHT = 768;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    SpriteBatch batch;
+    Texture img;
+
+    Viewport screenViewport = new FitViewport(VP_WIDTH, VP_HEIGHT);
+    Camera camera;
+
+    FrameBuffer frameBuffer_00;
+    FrameBuffer frameBuffer_01;
+    FrameBuffer frameBuffer_02;
+
+    ShaderProgram shaderMBlurPrev;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture(Gdx.files.internal("img/bullet-1.png"));
+
+        camera = new OrthographicCamera(VP_WIDTH, VP_HEIGHT);
+        camera.position.set(VP_WIDTH/2, VP_HEIGHT/2, 0);
+
+        frameBuffer_00 = new FrameBuffer(Pixmap.Format.RGBA8888,
+                (int) screenViewport.getWorldWidth(), (int) screenViewport.getWorldHeight(), false);
+        frameBuffer_01 = new FrameBuffer(Pixmap.Format.RGBA8888,
+                (int) screenViewport.getWorldWidth(), (int) screenViewport.getWorldHeight(), false);
+        frameBuffer_02 = new FrameBuffer(Pixmap.Format.RGBA8888,
+                (int) screenViewport.getWorldWidth(), (int) screenViewport.getWorldHeight(), false);
+
+        shaderMBlurPrev = new ShaderProgram(
+                Gdx.files.internal("shaders/postfx_vertex_00.glsl"),
+                Gdx.files.internal("shaders/postfx_fragment_mblurPrev.glsl")
+        );
+    }
+
+    void drawBG() {
+        batch.draw(img, 0, 0);
+    }
+
+    void drawCurrentBullets() {
+        batch.draw(img, 0, TimeUtils.millis() % 1000, 64, 64);
+    }
+
+    void drawPlanets() {
+
+    }
+
+    void drawAll() {
+        camera.update();
+        batch.setProjectionMatrix(camera.projection);
+        batch.setTransformMatrix(camera.view);
+
+        {
+            FrameBuffer prevBulletBuf = frameBuffer_00;
+            FrameBuffer curBulletBuf = frameBuffer_01;
+
+            curBulletBuf.begin();
+            glViewport(0, 0, curBulletBuf.getWidth(), curBulletBuf.getHeight());
+            Gdx.gl.glClearColor(0,0,0,0);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            batch.begin();
+            batch.setBlendFunction(GL_SRC_ALPHA, GL_ONE);
+            float bf = 0.95f;
+            batch.setColor(bf, bf, bf, 1);
+            batch.setShader(shaderMBlurPrev);
+            batch.draw(prevBulletBuf.getColorBufferTexture(), 0, 0);
+            batch.setColor(Color.WHITE);
+            batch.setShader(null);
+            drawCurrentBullets();
+            batch.setBlendFunction(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            batch.end();
+            curBulletBuf.end();
+
+            frameBuffer_00 = curBulletBuf;
+            frameBuffer_01 = prevBulletBuf;
+        }
+
+        {
+            frameBuffer_02.begin();
+            glViewport(0, 0, frameBuffer_02.getWidth(), frameBuffer_02.getHeight());
+            Gdx.gl.glClearColor(0,0,.2f,0);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            batch.begin();
+            drawBG();
+            batch.setBlendFunction(GL_SRC_ALPHA, GL_ONE);
+            batch.draw(frameBuffer_00.getColorBufferTexture(), 0, 0);
+            batch.setBlendFunction(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            drawPlanets();
+            batch.end();
+            frameBuffer_02.end();
+        }
+
+        {
+            screenViewport.apply(false);
+            Gdx.gl.glClearColor(0,0,0,0);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            batch.begin();
+            batch.draw(frameBuffer_02.getColorBufferTexture(), 0, 0);
+            batch.end();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        screenViewport.update(width, height);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0,0,0,0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        drawAll();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
