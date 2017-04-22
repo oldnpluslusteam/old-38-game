b8120d2867c62695732f5e1c1f5b5441e432f458
A.Bond <alexey.bond.94.55@gmail.com>
2017-04-22 11:43:34 +0600

    Initial commit.

---
+++ desktop/src/com/github/oldnpluslusteam/old_38_game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.oldnpluslusteam.old_38_game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.oldnpluslusteam.old_38_game.TheGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheGame(), config);
+	}
+}
---
+++ html/src/com/github/oldnpluslusteam/old_38_game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.github.oldnpluslusteam.old_38_game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.oldnpluslusteam.old_38_game.TheGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TheGame();
+        }
+}
